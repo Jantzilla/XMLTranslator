5cb118de216d78f72d1fa390eab74e38336bcc31
Jantz <jantzillac.93@gmail.com>
2019-03-20 12:16:17 -0500

    Remove "Android Comments" & change "Mime Type"

--- app/src/main/java/com/jantzapps/jantz/xmltranslatorfree/MainActivity.java
+++ app/src/main/java/com/jantzapps/jantz/xmltranslatorfree/MainActivity.java
@@ -1009,19 +1009,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     public void performFileSearch() {
 
-        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
-        // browser.
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-
-        // Filter to only show results that can be "opened", such as a
-        // file (as opposed to a list of contacts or timezones)
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        // Filter to show only images, using the image MIME data type.
-        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
-        // To search for all documents available via installed storage providers,
-        // it would be "*/*".
-        intent.setType("image/*");
+        intent.setType("text/xml");
 
         startActivityForResult(intent, READ_REQUEST_CODE);
     }
