49f3230f642c8e3f30323a998252005b47f9eaa0
Jantz <jantzillac.93@gmail.com>
2019-03-21 07:35:02 -0500

    Add method "reconnectDrive()" for "Google Drive Reconnection"

--- app/src/main/java/com/jantzapps/jantz/xmltranslatorfree/SettingsActivity.java
+++ app/src/main/java/com/jantzapps/jantz/xmltranslatorfree/SettingsActivity.java
@@ -1,14 +1,27 @@
 package com.jantzapps.jantz.xmltranslatorfree;
 
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class SettingsActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
+import static com.jantzapps.jantz.xmltranslatorfree.MainActivity.REQUEST_CODE_RESOLUTION;
+
+public class SettingsActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private Toolbar toolbar;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +41,37 @@ public class SettingsActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void reconnectDrive() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addScope(Drive.SCOPE_APPFOLDER)                                                     // required for App Folder sample
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override public void onConnected(@Nullable Bundle bundle) {}
+
+    @Override public void onConnectionSuspended(int i) {}
+
+    @Override public void onConnectionFailed(@NonNull ConnectionResult result) {
+        if (!result.hasResolution()) {
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
+            return;
+        }
+        try {
+            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        } catch (IntentSender.SendIntentException e) {}
+    }
+
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_RESOLUTION && resultCode == RESULT_OK) {
+            mGoogleApiClient.connect();
+        }
     }
 }
