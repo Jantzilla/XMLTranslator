ea0e48d6e630b9d02b9ecee1b1a1c8f5c8e4b7b7
Jantz <jantzillac.93@gmail.com>
2019-03-24 11:14:57 -0500

    Add "onStartCommand()" to service

--- app/src/main/java/com/jantzapps/jantz/xmltranslatorfree/utils/TranslationService.java
+++ app/src/main/java/com/jantzapps/jantz/xmltranslatorfree/utils/TranslationService.java
@@ -13,4 +13,10 @@ public class TranslationService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return START_STICKY;
+    }
 }
