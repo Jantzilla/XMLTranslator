1ff7a789d3bb05aef904a8a4d2178f7e6b8921cd
Jantz <jantzillac.93@gmail.com>
2019-05-09 23:54:36 -0500

    Change "ConfirmationDialog.java" to "ConfirmationDialog.kt"

---
+++ app/src/main/java/com/jantzapps/jantz/xmltranslatorfree/fragments/ConfirmationDialog.kt
@@ -0,0 +1,43 @@
+package com.jantzapps.jantz.xmltranslatorfree.fragments
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.CompoundButton
+
+import com.jantzapps.jantz.xmltranslatorfree.R
+
+class ConfirmationDialog : DialogFragment() {
+
+    var checkBox: CheckBox
+    private var button1: Button? = null
+    private var button2: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val mainView = inflater.inflate(R.layout.confirmation_dialog, container)
+        checkBox = mainView.findViewById<View>(R.id.checkBox) as CheckBox
+        button1 = mainView.findViewById<View>(R.id.button1) as Button
+        button2 = mainView.findViewById<View>(R.id.button2) as Button
+        return mainView
+    }
+
+    fun setPositiveButton(clickListener: View.OnClickListener) {
+        button1!!.setOnClickListener(clickListener)
+    }
+
+    fun setNegativeButton(clickListener: View.OnClickListener) {
+        button2!!.setOnClickListener(clickListener)
+    }
+
+    fun setOnCheckedChangeListener(checkedChangeListener: CompoundButton.OnCheckedChangeListener) {
+        checkBox.setOnCheckedChangeListener(checkedChangeListener)
+    }
+}
